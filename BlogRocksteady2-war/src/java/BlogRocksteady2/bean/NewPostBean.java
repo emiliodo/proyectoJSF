705ee56a784a1a98383b54d561b0fc25511d419d
Emilio <Emilio@epc>
2015-02-11 21:53:55 +0100

    carga de imagenes

--- BlogRocksteady2-war/src/java/BlogRocksteady2/bean/NewPostBean.java
+++ BlogRocksteady2-war/src/java/BlogRocksteady2/bean/NewPostBean.java
@@ -19,6 +19,7 @@ import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.servlet.http.Part;
+import org.apache.commons.io.IOUtils;
 
 /**
  *
@@ -27,11 +28,12 @@ import javax.servlet.http.Part;
 @ManagedBean
 @RequestScoped
 public class NewPostBean {
+
     @EJB
     private UsuarioFacade usuarioFacade;
     @EJB
     private PostFacade postFacade;
-    
+
     private String title;
     private Part headerImage;
     private String content;
@@ -78,37 +80,39 @@ public class NewPostBean {
     private String latitude;
     private String longitude;
 
-
     /**
      * Creates a new instance of NewPostBean
      */
     public NewPostBean() {
     }
-    
-    public String createPost(){
+
+    public String createPost() {
         
         //Controlar que el usuario logueado tiene permisos para crear Post
         Post newPost = new Post();
         newPost.setMvpost(Character.MIN_VALUE);
         newPost.setPostContent(content);
-        if (headerImage!=null){
-//            InputStream is;
-//            try {
-//                is = headerImage.getInputStream();
-//                byte[] img = IOUtils.toByteArray(is);
-//            } catch (IOException ex) {
-//                Logger.getLogger(NewPostBean.class.getName()).log(Level.SEVERE, null, ex);
-//            }
+
+        if (headerImage != null) {
+            InputStream is;
+            try {
+                is = headerImage.getInputStream();
+                byte[] img = IOUtils.toByteArray(is);
+                newPost.setHeaderImage(img);
+            } catch (IOException ex) {
+                Logger.getLogger(NewPostBean.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
+
         newPost.setTitle(title);
-        newPost.setPostGps(latitude+","+longitude);
-        
+        newPost.setPostGps(latitude + "," + longitude);
+
         //El usuario que tiene la sesion abierta.
         newPost.setPostedBy(usuarioFacade.find(new BigDecimal(BigInteger.ONE)));
         newPost.setPostDate(Calendar.getInstance().getTime());
         postFacade.create(newPost);
-        
+
         return ("blog.xhtml");
     }
-    
+
 }
