e83129188128a3c6d50b9921483bd0068c68c43d
Blackproxy <Blackproxy@Blackproxy-PC>
2015-02-11 22:07:10 +0100

    La imagen por fin se muestra

--- BlogRocksteady2-war/src/java/BlogRocksteady2/bean/PostBean.java
+++ BlogRocksteady2-war/src/java/BlogRocksteady2/bean/PostBean.java
@@ -6,13 +6,13 @@ import BlogRocksteady2.entity.Comentario;
 import BlogRocksteady2.entity.Post;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseId;
 import javax.servlet.annotation.MultipartConfig;
@@ -20,7 +20,7 @@ import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 
 @ManagedBean
-@SessionScoped
+@RequestScoped
 @MultipartConfig
 public class PostBean {
 
@@ -75,16 +75,16 @@ public class PostBean {
     public StreamedContent getImage() {
         
         FacesContext context = FacesContext.getCurrentInstance();
-        //context.getCurrentPhaseId()== PhaseId.RENDER_RESPONSE
-        if (context.getRenderResponse()) {
+//        context.getCurrentPhaseId()== PhaseId.RENDER_RESPONSE;
+        if (context.getCurrentPhaseId()== PhaseId.RENDER_RESPONSE) {
             // So, we're rendering the view. Return a stub StreamedContent so that it will generate right URL.
             return new DefaultStreamedContent();
-        }
+       }
         else {
             // So, browser is requesting the image. Get ID value from actual request param.
-            String idpost = context.getExternalContext().getRequestParameterMap().get("paramImage");
-            byte [] img = this.postFacade.find(idpost).getHeaderImage();
-            return new DefaultStreamedContent(new ByteArrayInputStream(img),"image/jpeg");
+            String idpost = context.getExternalContext().getRequestParameterMap().get("idpost");
+            byte [] img = this.postFacade.find(new BigDecimal(idpost)).getHeaderImage();
+            return new DefaultStreamedContent(new ByteArrayInputStream(img));
         }
     }
     public void setImage(StreamedContent img) {
