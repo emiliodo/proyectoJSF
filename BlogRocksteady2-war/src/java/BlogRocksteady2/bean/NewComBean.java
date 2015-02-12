77729808d59aa3d0d64826ba6107d031e878877c
Emilio <Emilio@epc>
2015-02-12 18:38:50 +0100

    comentarios2

--- BlogRocksteady2-war/src/java/BlogRocksteady2/bean/NewComBean.java
+++ BlogRocksteady2-war/src/java/BlogRocksteady2/bean/NewComBean.java
@@ -9,6 +9,7 @@ import BlogRocksteady2.ejb.ComentarioFacade;
 import BlogRocksteady2.ejb.PostFacade;
 import BlogRocksteady2.ejb.UsuarioFacade;
 import BlogRocksteady2.entity.Comentario;
+import BlogRocksteady2.entity.Post;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Calendar;
@@ -34,6 +35,15 @@ public class NewComBean {
     private ComentarioFacade comentarioFacade;
 
     private String contenidoComentario;
+    private Post postComentado;
+
+    public Post getPostComentado() {
+        return postComentado;
+    }
+
+    public void setPostComentado(Post postComentado) {
+        this.postComentado = postComentado;
+    }
 
     public String getContenidoComentario() {
         return contenidoComentario;
@@ -43,15 +53,13 @@ public class NewComBean {
         this.contenidoComentario = contenidoComentario;
     }
 
-    public void createCommnt() {
+    public void createCommnt(Post commented) {
 
         Comentario newComentario = new Comentario();
         newComentario.setCommentContent(contenidoComentario);
-        //newComentario.setPostCommented(postFacade.find(this));
- 
+        newComentario.setPostCommented(commented);
         newComentario.setCommentBy(usuarioFacade.find(new BigDecimal(BigInteger.ONE)));
         newComentario.setCommentDate(Calendar.getInstance().getTime());
-        
         comentarioFacade.create(newComentario);
         
     }
